11c4e3fdad57a03b8c8709ccfcec25ef0e8aea43
Carl Jones <JonesC162@cardiff.ac.uk>
2017-02-03 09:54:32 +0000

    Adding enums for Rank and Suit.
    No methods, just values.
    
    Enums are top-level classes at the moment.
    
    TO DO: Review this decision

---
+++ src/main/java/com/nsa/sds2/playingcards/Rank.java
@@ -0,0 +1,12 @@
+package com.nsa.sds2.playingcards;
+
+/**
+ * Enum to represent the rank of a Playing Card from ACE to KING
+ */
+
+
+public enum Rank {
+
+    ACE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING;
+
+}
---
+++ src/main/java/com/nsa/sds2/playingcards/Suit.java
@@ -0,0 +1,9 @@
+package com.nsa.sds2.playingcards;
+
+/**
+ * Class to represent playing card Suit e.g Clubs.
+ * No order or precedent is implied by the enumeration i.e. clubs are not lower than diamonds.
+ */
+public enum Suit {
+    CLUBS, DIAMONDS, HEARTS, SPADES
+}
